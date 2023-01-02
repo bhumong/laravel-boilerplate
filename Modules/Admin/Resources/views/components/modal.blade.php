98e31b9c67cd6f6bd9f257ef301e46b8f1870a73
samuel <samuel.bhumong@gmail.com>
2023-01-02 15:06:52 +0700

    modal component

--- Modules/Admin/Resources/views/components/modal.blade.php
+++ Modules/Admin/Resources/views/components/modal.blade.php
@@ -1,20 +1,20 @@
-<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="delete-modal-label"
+<div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-labelledby="delete-{{$id}}"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="delete-modal-label">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+            @if (!empty($header))
+                <div class="modal-header">
+                    {{$header}}
+                </div>
+            @endif
             <div class="modal-body">
-                ...
+                {{$slot}}
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            @if (!empty($footer))
+                <div class="modal-footer">
+                    {{$footer}}
+                </div>
+            @endif
         </div>
     </div>
 </div>
