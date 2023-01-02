4833e66aebf407218d969b559a417fd0ff144b45
samuel <samuel.bhumong@gmail.com>
2023-01-02 18:28:31 +0700

    custom error page

---
+++ Modules/Admin/Resources/views/pages/error/unauthorized.blade.php
@@ -0,0 +1,15 @@
+@section('title', 'Unauthorized')
+
+@section('page_level_js')
+@endsection
+
+<x-admin::app-layout>
+    <br>
+    <x-admin::card>
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">Unauthorized</h1>
+            </div>
+        </div>
+    </x-admin::card>
+</x-admin::app-layout>
